730fc9e47de4bc5eaf3c1f4d67aefebd1164c8fb
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-05-13 17:43:24 +0900

    List, Knock削除機能の実装

---
+++ src/components/atoms/RemoveListButton.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="removeEvent">
+      <slot />
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RemoveListButton',
+
+  methods: {
+    removeEvent () {
+      this.$emit('remove-event')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/components/templates/KnockDetailView.vue
+++ src/components/templates/KnockDetailView.vue
@@ -5,9 +5,20 @@
     <ListPreview
       :list="list"
     />
+    <!-- Knockを表示 -->
     <KnockPreview
       :knocks="knocks"
     />
+    <!-- 編集するボタンを表示 -->
+    <!-- <EditListButton/> -->
+    <!-- 削除するボタンを表示 -->
+    <div v-if="isshow">
+      <RemoveListButton
+        @remove-event="removeList"
+      >
+        削除する
+      </RemoveListButton>
+    </div>
   </div>
 </template>
 
@@ -17,6 +28,8 @@ import firebase from 'firebase'
 import UpdateLoginUser from '@/components/mixin/UpdateLoginUser'
 import ListPreview from '@/components/molecules/ListPreview'
 import KnockPreview from '@/components/molecules/KnockPreview'
+import EditListButton from '@/components/atoms/EditListButton'
+import RemoveListButton from '@/components/atoms/RemoveListButton'
 
 export default {
   name: 'KnockDetailView',
@@ -24,15 +37,19 @@ export default {
   components: {
     Navigation,
     ListPreview,
-    KnockPreview
+    KnockPreview,
+    EditListButton,
+    RemoveListButton
   },
   mixins: [UpdateLoginUser],
   data () {
     return {
       knocks: [],
-      list: {}
+      list: {},
+      isshow: false
     }
   },
+
   created: function () {
     // インスタンス作成後にリストのKnockを取得
     const db = firebase.firestore()
@@ -54,10 +71,47 @@ export default {
       .get().then((doc) => {
         this.list = doc.data()
         this.$store.dispatch('updateList', this.list)
+        // ログインユーザとリスト作成ユーザが一致してるか
+        let user = firebase.auth().currentUser
+        this.isshow = !!(this.list.user_id === user.uid)
       })
       .catch((err) => {
         console.log('Error getting list : ', err)
       })
+  },
+  methods: {
+    removeList () {
+      let knockDocs = []
+      const db = firebase.firestore()
+      // Listを削除する処理
+      // ListのDocument idからlistを削除
+      db.collection('list').doc(this.$route.params.listid)
+        .delete()
+        .catch((err) => {
+          console.log('Error deleting list doc id : ', err)
+        })
+      // ListのDocument idを持つKnockのDocument idを取得
+      db.collection('knock')
+        .where('list_id', '==', this.$route.params.listid).get()
+        .then((querySnapshot) => {
+          querySnapshot.forEach(function (doc) {
+            knockDocs.push(doc.id)
+          })
+          // 取得できた全てのknockのDocument idに対して削除を行う
+          for (let doc of knockDocs) {
+            db.collection('knock').doc(doc)
+              .delete()
+              .catch((err) => {
+                console.log('Error deleting knock doc id : ', err)
+              })
+          }
+          // 削除後、マイページに遷移
+          this.$router.push('/myKnockList')
+        })
+        .catch((err) => {
+          console.log('Error getting knock doc id : ', err)
+        })
+    }
   }
 }
 </script>
