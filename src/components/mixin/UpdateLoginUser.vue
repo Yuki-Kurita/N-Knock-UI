0d6bd4981439a0f8f25ff7782fac3f6e37aac149
Yuki Kurita <yukikur@yahoo-corp.jp>
2020-06-07 20:43:40 +0900

    Firebaseの認証が残り、stateがなくなった時にstateを更新

--- src/components/mixin/UpdateLoginUser.vue
+++ src/components/mixin/UpdateLoginUser.vue
@@ -6,10 +6,10 @@ export default {
   created: function () {
     // 現在ログインしており、Storeで認証情報が管理できてなければ更新する
     let user = firebase.auth().currentUser
-    if (user) {
-      let authInfo = {
-        'email': user.email
-      }
+    let loginUserEmail = this.$store.getters.loginUserEmail
+    if (user && !loginUserEmail) {
+      // emailからユーザ名を取得
+      let authInfo = {'email': user.email}
       this.$store.dispatch('login', authInfo)
         .catch(err => {
           console.log('update login user error', err)
--- src/components/templates/NKnockView.vue
+++ src/components/templates/NKnockView.vue
@@ -14,7 +14,7 @@
 
 <script>
 import Navigation from '@/components/molecules/Navigation.vue'
-// import UpdateLoginUser from '@/components/mixin/UpdateLoginUser.vue'
+import UpdateLoginUser from '@/components/mixin/UpdateLoginUser.vue'
 import firebase from 'firebase'
 
 export default {
@@ -22,7 +22,7 @@ export default {
   components: {
     Navigation
   },
-  // mixins: [UpdateLoginUser],
+  mixins: [UpdateLoginUser],
   data () {
     return {
       tags: []
